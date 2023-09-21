4f369eeb84ef33b603bc03886c4638d14e85f6c5
sergiohdljr <sergio.tjf1@gmail.com>
2023-09-21 16:11:18 -0300

    refactor: clean userController class

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,19 +1,18 @@
 import userService from "../services/userService";
-import { userType } from "../types/userTypes";
+import { userTypePayload } from "../types/userTypes";
 import { Request, Response } from "express";
 import { userExists } from "../utils/userExists";
 
 class UserController {
   async createUser(req: Request, res: Response) {
-    const payload = req.body as unknown as userType;
-    const username = payload.username;
+    const { username, password } = req.body as unknown as userTypePayload;
 
     if (await userExists(username)) {
       return res.sendStatus(409);
     }
 
     try {
-      await userService.saveUser(payload);
+      await userService.saveUser(username, password);
 
       return res.sendStatus(201);
     } catch (error) {
