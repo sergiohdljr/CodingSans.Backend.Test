1e2b86883e6c98ca6b2039e57ee7613fd0ddf083
sergiohdljr <sergio.tjf1@gmail.com>
2023-09-21 16:09:26 -0300

    feat: creating user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,27 @@
+class User {
+  private username: string;
+  private password: string;
+
+  constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
+
+  getUsername() {
+    return this.username;
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  setUsername(username: string) {
+    this.username = username;
+  }
+
+  setPassword(password: string) {
+    this.password = password;
+  }
+}
+
+export default User;
